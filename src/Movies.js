bf08fe090ea036dfa0a7ca15773edc4b34de0881
Mihir Yadav <72518307+mihir1729@users.noreply.github.com>
2022-09-20 23:35:47 +0530

    first commit

---
+++ src/Movies.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { useGlobalContext } from "./context";
+import { Link } from "react-router-dom";
+const url =
+	"https://upload.wikimedia.org/wikipedia/commons/f/fc/No_picture_available.png";
+
+const Movies = () => {
+	const { movies, isLoading } = useGlobalContext();
+
+	if (isLoading) {
+		return <div className='loading'></div>;
+	}
+
+	return (
+		<section className='movies'>
+			{/* {movies.map((movie) => {
+				console.log(movie);
+				return <h4>movie</h4>;
+			})} */}
+		</section>
+	);
+};
+
+export default Movies;
